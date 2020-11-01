3594b71a9f8c37ef6db3d3ff76b13f4a45bc0c7f
michellelin99 <michelle.lin.stuy@gmail.com>
2020-11-01 18:48:45 -0500

    added instagram request

---
+++ 2117_infinite_loop/js/instagram.js
@@ -0,0 +1,32 @@
+
+var token = 'IGQVJYM1FzTUkyd3dsVm9fTXpSODVRcG5pXzRnMGVndGpMTzBWSjRFWjg3dW4yRTZAhVFMyUHl1N0c4czJEdFl5VW5CTXRvUndFR1NuMXlFSnI2U0ZAUZAWNuWTZAyS3RXbE9veEFkNFlScDJwOVlSYkE0ZAAZDZD'
+      username = 'kelzthabarber'
+      num_photos = 12;
+
+  $.ajax({ // the first ajax request returns the ID of user kelzthabarber
+  	url: 'https://api.instagram.com/v1/users/search',
+  	dataType: 'jsonp',
+  	type: 'GET',
+  	data: {access_token: token, q: username}, // actually it is just the search by username
+  	success: function(data){
+  		console.log(data);
+  		$.ajax({
+  			url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent', // specify the ID of the first found user
+  			dataType: 'jsonp',
+  			type: 'GET',
+  			data: {access_token: token, count: num_photos},
+  			success: function(data2){
+  				console.log(data2);
+  				for(x in data2.data){
+            $('#img1').prop('src', data2.data[x].images.thumbnail.url);
+  				}
+      			},
+  			error: function(data2){
+  				console.log(data2);
+  			}
+  		});
+  	},
+  	error: function(data){
+  		console.log(data);
+  	}
+  });
