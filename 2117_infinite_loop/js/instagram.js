497f62daf8eb2440155d7d0a5ae86dd21ad8684a
michellelin99 <michelle.lin.stuy@gmail.com>
2020-11-01 23:58:31 -0500

    set today's color to blue

--- 2117_infinite_loop/js/instagram.js
+++ 2117_infinite_loop/js/instagram.js
@@ -1,3 +1,11 @@
+//set current day to light blue
+var currentDate = new Date();
+var day = currentDate.getDay();
+
+$(".d" + day).css("color", "#4287f5");
+$(".d" + day).css("font-weight", "Bold");
+
+
 //load temporary images into carousel
 var i;
 for(i = 1; i <= 8; ++i){
